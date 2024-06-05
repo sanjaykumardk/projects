962b325727ff41d9e56d57b9e1742d6e4be3bf85
Sanjay Kumar <145103929+sanjaykumardk@users.noreply.github.com>
2024-06-05 18:37:36 +0530

    Update cipher_encrypt_decrypt_with_GUI.py

--- Encrypt_Decrypt_GUI/cipher_encrypt_decrypt_with_GUI.py
+++ Encrypt_Decrypt_GUI/cipher_encrypt_decrypt_with_GUI.py
@@ -18,7 +18,105 @@ class CaesarCipher(tk.Frame):
     self.main_frame = self
     self.main_frame.pack(fill=tk.BOTH, expand=TRUE)
     self.main_frame.columnconfigure(0, weight=1)
+    self.render_widgets()
 
+  def render_widgets(self):
+    self.title = tk.Label(
+      self.main_frame,
+      bg=self.colour1,
+      fg=self.colour2,
+      font=('Arial',22,'bold')
+      text='Caesar Cipher'
+    )
+
+    self.title.grid(column=0,row=0,sticky=tk.Ew,pady=35)
+
+    self.text_widget = tk.Text(
+      self.main_frame,
+      bg=self.colour2,
+      fg=self.colour1,
+      selectbackground=self.colour1,
+      selectforeground=self.colour2,
+      font=('Arial',17),
+      height=5,
+      padx=10,
+      pady=10,
+      highlightthickness=0,
+      border=0
+    )
+
+    self.text_widget.grid(column=0, row=1, padx=100)
+
+    self.key_label = tk.Label(
+      self.main_frame,
+      bg=self.colour1,
+      fg=self.colour2,
+      font=('Arial',13)
+      text=f'Key(1-{self.num_letters-1})',
+      justify=tk.CENTER
+    )
+
+    self.key_label.grid(column=0,row=2,pady=(38,10))
+
+    self.buttons_container=tk.Frame(self.main_frame, bg=self.colour1)
+    self.buttons_container.columnconfigure(0,weight=1)
+    self.buttons_container.columnconfigure(1,weight=1)
+    self.buttons_container.columnconfigure(2,weight=1)
+    
+    self.buttons_container.grid(column=0,row=3,sticky=tk.NSEW, padx=100)
+
+    self.button_encrypt = tk.Button(
+      self.buttons_container,
+      bg=self.colour2,
+      fg=self.colour1,
+      activebackground=self.colour3,
+      activeforeground=self.colour1,
+      font=('Arial',15),
+      text='Encrypt',
+      width=6,
+      height=1,
+      cursor='hand1'
+      highlightthickness=0,
+      border=0,
+      state=tk.DISABLED
+    )
+
+    self.button_encrypt.grid(column=0, row=0, ipadx=5, ipady=5)
+
+    
+    self.button_decrypt = tk.Button(
+      self.buttons_container,
+      bg=self.colour2,
+      fg=self.colour1,
+      activebackground=self.colour3,
+      activeforeground=self.colour1,
+      font=('Arial',15),
+      text='Decrypt',
+      width=6,
+      height=1,
+      cursor='hand1'
+      highlightthickness=0,
+      border=0,
+      state=tk.DISABLED
+    )
+
+    self.button_decrypt.grid(column=2, row=0, ipadx=5, ipady=5)
+
+    self.key_entry = tk.Entry(
+      self.buttons_container,
+      bg=self.colour2,
+      fg=self.colour1,
+      selectbackground=self.colour1,
+      selectforeground=self.colour2,
+      font=('Arial',15),
+      width=6,
+      highlightthickness=0,
+      border=0,
+      justify=tk.CENTER
+    )
+
+    self.key_entry.grid(column=1, row=0, ipady=9)
+      
 operating_system = sys.platform
 root = tk.Tk()
 caesar_cipher_app = CaesarCipher(root)
@@ -33,3 +131,4 @@ elif 'darwin' in operating_system:
 
 root.resizable(width=False, height=False)
 root.mainloop()
+
